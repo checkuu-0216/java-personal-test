0176b801ec9dd0fef920af7f2ddfac541b43dfc1
checkuu-0216 <corb37@gmail.com>
2024-07-24 22:23:32 +0900

    1-7 "remove" 입력시 첫번째 배열 삭제

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,6 +12,7 @@ public class App {
         Queue<Integer> intQueue = new LinkedList<>(); //앞선 결과를 삭제하고 새로운 연산결과를 저장하기위한 컬랙션
         int count = 0; //카운트 세기위한 index
         while (true) { //결과값을 배열에 넣기위해 while에서 for문으로 바꿈
+
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt(); //입력된 숫자를 저장하기 위한 nextInt
             System.out.print("사칙연산 기호를 입력하세요: ");
@@ -44,23 +45,24 @@ public class App {
                     //사칙연산의 switch인데 다른문자가 들어 갔을 경우를 위해 작성
             }
             System.out.println("결과: " + result);
-            if (count > 9) {
-                intQueue.poll();
-                intQueue.add(result); //10개 쌓인 후 앞에 지우고 값넣기 위한 식
-            } else {
-                intQueue.add(result); //10개전까진 그대로 쌓이게 만듬
-            }
+
 
             count++;
             System.out.println(count + "번 계산하였습니다."); //while이 돌아간 수를 세기 위함
-
+            intQueue.add(result);
             System.out.println("계산 기록 : " + intQueue); //계산기록 배열
 
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String text = sc.next();
+            if (text.equals("remove")) {
+                intQueue.poll();
+            }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String text = sc.next(); //계산종료 확인을 위한 텍스트 입력 구간
+            sc.next(); //계산종료 확인을 위한 텍스트 입력 구간
             if (text.equals("exit")) {
                 break; // 강의 2주차 숙제를 통해 특정 문자를 사용했을때 종료되는 법 배움
             }
         }
     }
 }
+
