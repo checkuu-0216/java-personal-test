0c73f12cceff6be4e7a1a1ed268cf265d2f6a4ee
checkuu-0216 <corb37@gmail.com>
2024-07-24 13:04:08 +0900

    1-2 사칙연산 기호 전달받기 테스트

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,10 +8,11 @@ public class App {
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt(); //입력된 숫자를 저장하기 위한 nextInt
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0); //사칙연산 기호를 넣기 위한 메소드
+        // charAt 매개변수로 char 타입으로 변환 하고자하는 문자열의 위치를 받는다
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
-
-        System.out.println("num1, num2 = " + num1 + " , " + num2);
 
     }
 }
