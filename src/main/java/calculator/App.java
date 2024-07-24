0d3c2a98923dd7e6458b54ad61df176eb3ef56a3
checkuu-0216 <corb37@gmail.com>
2024-07-24 14:10:30 +0900

    1-4 반복문을 사용하여 특정 문자 입력전까지 무한반복 테스트

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        while (true) { // 무한루프를 만들기 위해 조건에 true를 넣었다. 전체 사이클을 반복하기 위해 while로 전부 감쌌다.
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt(); //입력된 숫자를 저장하기 위한 nextInt
@@ -28,15 +29,23 @@ public class App {
                 break;
             case '/':
                 // 나눗셈 연산에서 분모가 0일 경우를 위한 if 문
-                if(num2 ==0){
+                if (num2 == 0) {
                     System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
-                }else{
-                result = num1 / num2;}
+                } else {
+                    result = num1 / num2;
+                }
                 break;
             default:
                 System.out.println("사칙 연산외에 다른 기호가 들어갈 수 없습니다.");
                 //사칙연산의 switch인데 다른문자가 들어 갔을 경우를 위해 작성
         }
         System.out.println("결과: " + result);
+
+        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String text = sc.next(); //계산종료 확인을 위한 텍스트 입력 구간
+            if (text.equals("exit")) {
+                break; // 강의 2주차 숙제를 통해 특정 문자를 사용했을때 종료되는 법 배움
+            }
+        }
     }
 }
